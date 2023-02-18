d687fe16c12405f4eb3bcf1dae6d07201434c8a0
KapsJ <92434610+KayJay68@users.noreply.github.com>
2023-02-18 13:22:51 +0200

    Edit

--- variables.tf
+++ variables.tf
@@ -2,9 +2,3 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.micro"
 }
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.19.0"
-  cidr_block = "10.100.0.0/16"
-}
