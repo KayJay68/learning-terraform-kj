9428cbf9a4872ac17b335b7fa077a5a84605e181
KapsJ <92434610+KayJay68@users.noreply.github.com>
2023-02-18 13:17:41 +0200

    Added VPC

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.micro"
 }
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+}
