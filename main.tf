67753a3c436d0554bd2009138ceb1286344d0dc0
KapsJ <92434610+KayJay68@users.noreply.github.com>
2023-02-18 13:05:16 +0200

    Changed VPC - Default

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ module "vpc" {
   create_vpc = false
 
   manage_default_vpc               = true
-  default_vpc_name                 = "defaultVPC Didasko"
+  default_vpc_name                 = "VPC Didasko"
   default_vpc_enable_dns_hostnames = true
 
   tags = local.tags
